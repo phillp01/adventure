1037237ca23c76a672e42e80c0906e98fdf3f232
Paul <paul@kingofthemoon.co.uk>
2016-12-11 16:26:13 +0000

    Added inventory and items to pick up.

--- adventure.py
+++ adventure.py
@@ -1,4 +1,4 @@
-from data import locations
+from data import *
 
 directions = {
     'west':(-1,0),
@@ -10,18 +10,40 @@ directions = {
 position = (0,0)
 
 while True:
-    location = locations[position]
-    print 'you are at the %s' % location
+    location = locations[position]['pName']
+    noOfItems = len(locations[position]['Objects'])
+    items = locations[position]['Objects']
 
+    print 'you are at the %s - %s\n\nThere are %s items you can pick up.' % (location, locations[position]['Description'],noOfItems)
+    for x in items:
+        print 'You can pick up the %s' % (x)
+
+    print '\n'
     valid_directions = {}
     for k, v in directions.iteritems():
         possible_position = (position[0] + v[0], position[1] + v[1])
-        possible__location = locations.get(possible_position)
+        possible__location = locations.get(possible_position,{}).get('pName')
         if possible__location:
             print 'to the %s is a %s' % (k, possible__location)
             valid_directions[k] = possible_position
 
-    direction = raw_input('which direction to you want to go?\n')
-    position = valid_directions[direction]
+    direction = raw_input("\nType the direction to you want to go? Or type 'Pickup (item)' to pickup an item\n")
+
+    command = direction.partition(' ')[0]
+
+    if len(direction.split()) > 1:
+        item = direction.split(' ')[1].lower()
+
+    if command == 'pickup' or command == 'drop':
+        item_action(items,item,command)
+    elif command in valid_directions:
+        position = valid_directions[direction]
+    elif command == 'inventory':
+        show_inventory()
+    else:
+        print 'This is not a Valid Option'
+
+
+
 
 
--- data.py
+++ data.py
@@ -1,6 +1,46 @@
+# locations = {
+#     (0,0): 'House',
+#     (0,1): 'Lake',
+#     (1,0): 'Park',
+#     (1,1): 'Market',
+# }
+
 locations = {
-    (0,0): 'house',
-    (0,1): 'lake',
-    (1,0): 'park',
-    (1,1): 'market',
+    (0,0): {'pName':'House','map':'H','Description':'A lovely Venecian Mannor','Objects':['keys','coat']},
+    (0,1): {'pName':'Lake','map':'L','Description':"It's Deep - Oh So Deep",'Objects':['fishing rod','lilly']},
+    (1,0): {'pName':'Park','map':'P','Description':'Green, with trees and grass','Objects':['flowers','grass']},
+    (1,1): {'pName':'Market','map':'M','Description':'Sundays and Tuesdays Only!!','Objects':['spices','toys']},
 }
+
+
+inventory = []
+
+
+def item_action(items, item, action):
+
+    if action == 'pickup':
+
+        if item in items:
+            items.remove(item)
+            inventory.append(item)
+
+            print 'You have picked up the %s\n' % item
+
+        else:
+            print 'You can not see this item'
+            show_inventory()
+
+    if action == 'drop':
+
+        if item in inventory:
+            inventory.remove(item)
+            items.append(item)
+
+            print 'You have dropped the %s\n\n' % item
+            show_inventory()
+
+        else:
+            print 'you are not holding this item'
+
+def show_inventory():
+    print "You are currently carrying : %s" % inventory
